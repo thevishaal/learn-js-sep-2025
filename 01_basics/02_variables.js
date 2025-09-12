a9cc06aab5a95ee9ebdfea5d865c200e4d9ccb57
Vishal Baghel <thevishaal0@gmail.com>
2025-09-12 14:19:28 +0000

    basic of variables and datatype

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code newer version
+
+// alert("vishal"); // we are using nodejs, not browser
+
+let name = "vishal"
+let age = 19
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// undefined =>
+// null => standalone value
+// symbol => unique
+
+//object
+
+console.log(typeof undefined) // undefined 
+console.log(typeof null)  // object 
