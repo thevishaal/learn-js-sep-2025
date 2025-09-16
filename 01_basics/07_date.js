cc729097ee5639dcd003e198cafb35b49ff9416d
Vishal Baghel <thevishaal0@gmail.com>
2025-09-16 12:10:06 +0000

    date and time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,37 @@
+// Dates 
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2025, 8,16)
+// let myCreatedDate = new Date(2025, 8, 16, 5, 23)
+// let myCreatedDate = new Date("2025-09-16")
+// let myCreatedDate = new Date("09-16-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time...`
+
+// newDate.toLocaleString('default',{
+//     weekday: "long"
+    
+// })
+
+
+
+
+
+
