c787be0105bee65922958e88c919dce9f1407dcd
Vishal Baghel <thevishaal0@gmail.com>
2025-09-19 04:40:03 +0000

    scope level and mini hoisting

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,61 @@
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // var c = 30
+    // console.log("Inner:", a);   
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "vishal"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "vishal"
+    if (true) {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++ interesting ++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
